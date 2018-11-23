4d433af7e8a4653ea93135232931a91d3a17c2e7
sato-mmello <guilherme.sato.mmello@gmail.com>
2018-11-23 15:04:03 -0200

    Selenium remove similar category

--- Vtex API/selenium_similarcategory.py
+++ Vtex API/selenium_similarcategory.py
@@ -3,8 +3,9 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
 import time
-from database.database_connection import DatabaseConnection
+from shadow_database import DatabaseConnection
 dc = DatabaseConnection()
 import os, fnmatch, shutil, requests, csv
 from bs4 import BeautifulSoup as Soup
@@ -23,23 +24,25 @@ product_fail_selenium = []
 # 	group by ps.produto, v_item.product_id
 # 	;
 # """
-products_to_modify = ['530212','531792','531834','531809','531799','531827','531795','531247','531798','531811','531810','529963','530774','531805','530796','530974','531564','530897','531763','531266','530951','531678','531301','531837','530952','531184','528024','531294','530993','530808','530740','530809','531297','530954','530960','530955','531287','531465','530981','531374','531775','530980','531067','531774','531199','530979']
+products_to_modify = ['530656']
 # products_to_modify = dc.select(query, trim=True)
 # raise Exception(len(products_to_modify))
 
 timeout = 20
-exclude_var = 'Black Friday 33 50 66'
+exclude_var = 'Integracao'
 path ="C:/geckodriver.exe"
 
-driver = webdriver.Firefox(executable_path=path)
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options, executable_path=path)
 driver.get("https://marciamello.myvtex.com/_v/auth-server/v1/login?ReturnUrl=%2Fadmin%2F")
 
 action = driver.find_element_by_id("vtexIdUI-google-plus")
 action.click()
 
 window_handles = driver.window_handles
-driver.switch_to_window(window_handles[1])
-
+driver.switch_to.window(window_handles[1])
+print("Authenticating...")
 element_present = EC.presence_of_element_located((By.ID, 'identifierId'))
 WebDriverWait(driver, timeout).until(element_present)
 
@@ -47,30 +50,30 @@ identifier_id = driver.find_element_by_id("identifierId")
 identifier_id.send_keys("guilherme.sato.mmello@gmail.com")
 action = driver.find_element_by_id("identifierNext")
 action.click()
-
-time.sleep(5)
+print("Login id validated")
+time.sleep(3)
 identifier_id = driver.find_element_by_xpath("//input[@class='whsOnd zHQkBf']")
 identifier_id.send_keys("#googlemm")
 
 action = driver.find_element_by_id("passwordNext")
 action.click()
-
-driver.switch_to_window(window_handles[0])
+print("Password validated")
+driver.switch_to.window(window_handles[0])
 
 for product_info in products_to_modify:
 	for i in range(0,3):
 		try:
 			product_id = product_info
+			print(product_id)
 			# product_id = product_info['product_id']
-
 			time.sleep(5)
 			driver.get("https://marciamello.vtexcommercestable.com.br/admin/Site/ProdutoCategoriaSimilar.aspx?IdProduto=%s" % product_id) 
 			element_present = EC.presence_of_element_located((By.CSS_SELECTOR, "div#content table tr td"))
 			WebDriverWait(driver, timeout).until(element_present)
 			table_id = driver.find_element_by_css_selector('div#content table')
 			rows = table_id.find_elements(By.TAG_NAME, "tr")[1:]
-			print(len(rows))
-			print(table_id)
+			# print(len(rows))
+			# print(table_id)
 			for i,row in enumerate(rows,1):
 				cols = row.find_elements(By.TAG_NAME, "th")
 				for col in cols:
@@ -85,7 +88,7 @@ for product_info in products_to_modify:
 			            	row.find_element_by_css_selector('div#content table td a span').click()
 			            	alert = driver.switch_to_alert()
 			            	alert.accept()
-
+			            	print('Removed')
 			            	product_succeed_selenium.append(product_id)
 			break
 
@@ -94,7 +97,7 @@ for product_info in products_to_modify:
 			if i == retry-1:
 				product_fail_selenium.append(product_id)
 				print('error')
-			            	
+driver.quit()
 with open('selenium_succeed2.csv', 'w') as f:
 	csv_file = csv.writer(f, delimiter=";")
 	for n in product_succeed_selenium:
