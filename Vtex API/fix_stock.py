bed9f480b713a121a494dec42442c71d5011f1e7
Felipe <kibe.felipe@gmail.com>
2018-12-03 14:38:48 -0200

    pegando do json

--- Vtex API/fix_stock.py
+++ Vtex API/fix_stock.py
@@ -1,10 +1,10 @@
-import requests, json, dateutil.relativedelta
-from datetime import datetime, timedelta
 from shadow_database import DatabaseConnection
 from shadow_helpers.helpers import make_dict
 from shadow_vtex.vtex import try_to_request
-from multiprocessing import Pool, Manager
 
+from datetime import datetime, timedelta
+from multiprocessing import Pool, Manager
+import requests, json, dateutil.relativedelta
 
 def format_int_to_float(int_value):
 	str_value = str(int_value)
@@ -39,108 +39,8 @@ ORDER_STATUS = {
 	'canceled': 'Cancelado',
 }
 
-# api_connection_file = open("api_connection.json", 'rb')
-# api_connection_config = json.load(api_connection_file)
-
-api_connection_config = {
-	"Content-Type": "application/json",
-	"X-VTEX-API-AppKey": "vtexappkey-marciamello-XNZFUX",
-	"X-VTEX-API-AppToken": "HJGVGUPUSMZSFYIHVPLJPFBZPYBNLCFHRYTTUTPZSYTYCHTIOPTJKAABHHFHTCIPGSAHFOMBZLRRMCXHFSYWJVWRXRLNOIGPPDSJHLDZCRKZJIPFKYBBDMFLVIKODZNQ"
-}
-# list_orders_url = 'http://marciamello.vtexcommercestable.com.br/api/oms/pvt/orders'
-
-# end_date = datetime.now()
-# start_date = datetime(year=end_date.year, month=end_date.month, day=end_date.day, hour=2, minute=0, second=0) - timedelta(days=8)
-
-# print('start_date: %s' % start_date)
-# print('end_date: %s' % end_date)
-
-# formatted_end_date = (end_date + timedelta(hours=2)).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
-# formatted_start_date = start_date.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
-# params = {
-# 	'orderBy': 'creationDate,desc',
-# 	'per_page': '15',
-# 	'f_creationDate': 'creationDate:[%sZ TO %sZ]' % (formatted_start_date, formatted_end_date)
-# }
-
-
-# print('creationDate:[¨%sZ TO %sZ]' % (formatted_start_date, formatted_end_date))
-
-# with open('order_output.csv', 'wb') as f:
-# order_items = []
-# total_items = 1
-# skus_to_reduce_bypass = []
-
-# c=0
-# while (c*15) < total_items:
-# 	c+=1
-# 	print(u"Page: %d" % c, end='')
-# 	params["page"] = c
-# 	response = requests.request("GET", list_orders_url, headers=api_connection_config, params=params)
-
-# 	response_status_code = response.status_code
-
-# 	print(u"   status code: %d" % response_status_code)
-# 	list_json_response = json.loads(response.text)
-
-# 	for order_json in list_json_response['list']:
-# 		order_id = order_json['orderId']
-
-# 		for i in range(0,10):
-# 			order_response = requests.request("GET", list_orders_url + '/' + order_id, headers=api_connection_config)
-# 			order_json_response = json.loads(order_response.text)
-
-# 			if order_json_response.get('error'):
-# 				continue
-
-# 			break
-
-# 		order_info = []
-# 		order_info.append(order_id) # order_id
-# 		order_info.append(int(order_json_response['sequence'])) # sequence
-# 		created_at = datetime.strptime(order_json_response['creationDate'][:-14], '%Y-%m-%dT%H:%M:%S')
-# 		created_at = created_at - timedelta(hours=2)
-# 		order_info.append(created_at) # created_at
-# 		order_info.append(order_json_response['clientProfileData']['firstName'] + ' ' + order_json_response['clientProfileData']['lastName']) # client_name
-# 		order_info.append(ORDER_STATUS[order_json_response['status']]) # status
-
-# 		total_product_price = format_int_to_float(order_json_response['totals'][0]['value'] + order_json_response['totals'][1]['value'])
-# 		total_shipping_price = format_int_to_float(order_json_response['totals'][2]['value'])
-
-# 		order_info.append(total_product_price) # total_product_price
-# 		order_info.append(total_shipping_price) # total_shipping_price
-# 		order_info.append(total_product_price + total_shipping_price) # total_order_price
-
-# 		for item in order_json_response['items']:
-# 			order_item = order_info.copy()
-# 			order_item.append(item['ean']) # ean
-# 			order_item.append(int(item['id'])) # vtex_sku
-# 			order_item.append(int(item['productId'])) # vtex_product_id
-# 			order_item.append(item['quantity']) # quantity
-# 			order_item.append(item['name']) # name
-# 			order_item.append(format_int_to_float(item['price'])) # tb tem sellingPrice # price
-
-# 			order_items.append(order_item)
-
-# 			skus_to_reduce_bypass.append({
-# 				'sku_id': int(item['id']),
-# 				'ordered_quantity': item['quantity'],
-# 				'status': ORDER_STATUS[order_json_response['status']],
-# 			})
-
-# 	total_items = list_json_response['stats']['stats']['totalValue']['Count']
-
-
-# print('Connecting to database...',end='')
-# dc = DatabaseConnection()
-# print('Done!')
-
-# print('Inserting into tables...')
-
-# print('	vtex_order_items')
-# dc.execute('TRUNCATE TABLE vtex_order_items;')
-# dc.insert('vtex_order_items', order_items, print_only=False)
-
+api_connection_file = open("api_connection.json", 'rb')
+api_connection_config = json.load(api_connection_file)
 
 def get_stock_fix_from_linx(x):
 	sku = x[0] 
