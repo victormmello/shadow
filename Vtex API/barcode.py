acc8ac7134c4f2f3ba57e185b54dd6f308f8784f
sato-mmello <guilherme.sato.mmello@gmail.com>
2018-12-10 22:34:21 -0200

    barcode inserindo em bi_estoque_localizacao

--- Vtex API/barcode.py
+++ Vtex API/barcode.py
@@ -1,10 +1,11 @@
 import sys
+from shadow_database import DatabaseConnection
+from shadow_helpers.helpers import set_in_dict
+dc = DatabaseConnection()
 
 fp = open('/dev/hidraw0', 'rb')
 
 def barcode_reader():
-    """Barcode code obtained from 'brechmos' 
-    https://www.raspberrypi.org/forums/viewtopic.php?f=45&t=55100"""
     hid = {4: 'a', 5: 'b', 6: 'c', 7: 'd', 8: 'e', 9: 'f', 10: 'g', 11: 'h', 12: 'i', 13: 'j', 14: 'k', 15: 'l', 16: 'm',
            17: 'n', 18: 'o', 19: 'p', 20: 'q', 21: 'r', 22: 's', 23: 't', 24: 'u', 25: 'v', 26: 'w', 27: 'x', 28: 'y',
            29: 'z', 30: '1', 31: '2', 32: '3', 33: '4', 34: '5', 35: '6', 36: '7', 37: '8', 38: '9', 39: '0', 44: ' ',
@@ -15,56 +16,67 @@ def barcode_reader():
             29: 'Z', 30: '!', 31: '@', 32: '#', 33: '$', 34: '%', 35: '^', 36: '&', 37: '*', 38: '(', 39: ')', 44: ' ',
             45: '_', 46: '+', 47: '{', 48: '}', 49: '|', 51: ':', 52: '"', 53: '~', 54: '<', 55: '>', 56: '?'}
 
-    fp = open('/dev/hidraw0', 'rb')
+    
 
     ss = ""
     shift = False
-
     done = False
 
     while not done:
 
-        ## Get the character from the HID
         buffer = fp.read(8)
         for c in buffer:
             if ord(c) > 0:
 
-                ##  40 is carriage return which signifies
-                ##  we are done looking for characters
                 if int(ord(c)) == 40:
                     done = True
                     break;
 
-                ##  If we are shifted then we have to
-                ##  use the hid2 characters.
                 if shift:
 
-                    ## If it is a '2' then it is the shift key
                     if int(ord(c)) == 2:
                         shift = True
 
-                    ## if not a 2 then lookup the mapping
                     else:
                         ss += hid2[int(ord(c))]
                         shift = False
 
-                ##  If we are not shifted then use
-                ##  the hid characters
-
                 else:
-
-                    ## If it is a '2' then it is the shift key
                     if int(ord(c)) == 2:
                         shift = True
 
-                    ## if not a 2 then lookup the mapping
                     else:
                         ss += hid[int(ord(c))]
     return ss
 
 if __name__ == '__main__':
+    flag_position = False
+    flag_ean = False
 	try:
-        while True:
-            barcode_reader()
+		while True:
+			barcode = barcode_reader()
+            if (len(barcode)==4):
+                position = barcode
+                flag_position = True
+            elif barcode:
+                ean = barcode
+                flag_ean = True
+                
+            if flag_ean and flag_position:
+
+                flag_position = False
+                flag_ean = False
+
+                query = """
+                    INSERT INTO bi_estoque_localizacao (filial, codigo_barra, posicao)
+                    VALUES ('E-COMMERCE', %(codigo_barra)s, %(posicao)s) 
+                    """ % {
+                        "codigo_barra": ean,
+                        "posicao": position
+                    }
     except KeyboardInterrupt:
         pass
+
+
+
+		
