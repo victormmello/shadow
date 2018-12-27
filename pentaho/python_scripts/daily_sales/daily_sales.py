56f4fc89003b38af430ac64f5ace77fef2caee3f
Victor <victor@marciamello.com.br>
2018-12-27 11:44:26 -0200

    pentaho no shadow

---
+++ pentaho/python_scripts/daily_sales/daily_sales.py
@@ -0,0 +1,38 @@
+from shadow_database import DatabaseConnection
+import shadow_google_spreadsheet
+from multiprocessing import Pool
+
+WORKBOOK_NAME = 'Daily Sales'
+WORKSHEET_LIST = [
+	{'query_file':'daily_sales_sales_data_mtd.sql','worksheet_name':'SalesDataMTD'},
+	{'query_file':'daily_sales_sales_data_week.sql','worksheet_name':'SalesDataWEEK'},
+	{'query_file':'daily_sales_sellers_data_mtd.sql','worksheet_name':'SellerDataMTD'},
+	{'query_file':'daily_sales_sellers_data_week.sql','worksheet_name':'SellerDataWEEK'},
+	{'query_file':'daily_sales_sales_orders.sql','worksheet_name':'SalesOrders'},
+	{'query_file':'daily_sales_stock_data.sql','worksheet_name':'StockData'},
+	{'query_file':'daily_sales_stock_data_wo_store.sql','worksheet_name':'StockDataWOStore'},
+	{'query_file':'daily_sales_receivings_data.sql','worksheet_name':'ReceivingsData'},
+	{'query_file':'daily_sales_receivings_data_wo_store.sql','worksheet_name':'ReceivingsDataWOStore'},
+]
+
+workbook = shadow_google_spreadsheet.open(WORKBOOK_NAME)
+dc = DatabaseConnection()
+
+def get_and_set_data(worksheet_dict):
+	print('Updating sheet: %s...' % worksheet_dict['worksheet_name'],end='')
+	worksheet = workbook.worksheet(worksheet_dict['worksheet_name'])
+	query = open(worksheet_dict['query_file']).read()
+	query_result = dc.select(query,strip=True)
+	columns = [column[0] for column in dc.cursor.description]
+	shadow_google_spreadsheet.update_cells_with_dict(worksheet,columns,query_result)
+	print('Done!')
+
+if __name__ == '__main__':
+
+	# Single Thread:
+	# for worksheet_dict in WORKSHEET_LIST:
+		# get_and_set_data(worksheet_dict)
+
+	# Multi Threading:
+	with Pool(len(WORKSHEET_LIST)) as p:
+		result = p.map(get_and_set_data, WORKSHEET_LIST)
