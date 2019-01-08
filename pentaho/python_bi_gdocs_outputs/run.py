3518f8732f216c1cf58983e77bae361ad68283c5
Victor <victor@marciamello.com.br>
2019-01-08 17:05:13 -0200

    GDOCS Outputs

--- pentaho/python_bi_gdocs_outputs/run.py
+++ pentaho/python_bi_gdocs_outputs/run.py
@@ -1,30 +1,35 @@
 from shadow_database import DatabaseConnection
 import shadow_google_spreadsheet
 from multiprocessing import Pool
-import sys
+import sys, json
 
-PARAMETERS = {
-	"multi_thread":"True",
-	"max_threads":0, # 0 if threads = len(data)
-	"date_to":"GETDATE()-1",
-	"workbook_name":"Daily Sales",
-"worksheet_list":{
-	"daily_sales/sales_data_mtd.sql":"SalesDataMTD",
-	"daily_sales/sales_data_week.sql":"SalesDataWEEK",
-	"daily_sales/sellers_data_mtd.sql":"SellerDataMTD",
-	"daily_sales/sellers_data_week.sql":"SellerDataWEEK",
-	"daily_sales/sales_orders.sql":"SalesOrders",
-	"daily_sales/stock_data.sql":"StockData",
-	"daily_sales/stock_data_wo_store.sql":"StockDataWOStore",
-	"daily_sales/receivings_data.sql":"ReceivingsData",
-	"daily_sales/receivings_data_wo_store.sql":"ReceivingsDataWOStore"
-}
-}
+# PARAMETERS = {
+# 	"multi_thread":"True",
+# 	"max_threads":0, # 0 if threads = len(data)
+# 	"date_to":"GETDATE()-1",
+# 	"workbook_name":"Daily Sales",
+# 	"worksheet_list":{
+# 		"daily_sales/sales_data_mtd.sql":"SalesDataMTD",
+# 		"daily_sales/sales_data_week.sql":"SalesDataWEEK",
+# 		"daily_sales/sellers_data_mtd.sql":"SellerDataMTD",
+# 		"daily_sales/sellers_data_week.sql":"SellerDataWEEK",
+# 		"daily_sales/sales_orders.sql":"SalesOrders",
+# 		"daily_sales/stock_data.sql":"StockData",
+# 		"daily_sales/stock_data_wo_store.sql":"StockDataWOStore",
+# 		"daily_sales/receivings_data.sql":"ReceivingsData",
+# 		"daily_sales/receivings_data_wo_store.sql":"ReceivingsDataWOStore"
+# 	}
+# }
+MULTI_THREAD = sys.argv[1]
+MAX_THREADS = sys.argv[2]
+DATE_TO = sys.argv[3]
+WORKBOOK_NAME = sys.argv[4]
+WORKSHEET_LIST = json.load(sys.argv[5])
 
-def get_and_set_data(worksheet_dict):
-	print('Updating sheet: %s...' % worksheet_dict['worksheet_name'],end='')
-	worksheet = workbook.worksheet(worksheet_dict['worksheet_name'])
-	query = open(worksheet_dict['query_file']).read()  % {
+def get_and_set_data(worksheet_tuple):
+	print('Updating sheet: %s...' % worksheet_tuple[1],end='')
+	worksheet = workbook.worksheet(worksheet_tuple[1])
+	query = open(worksheet_tuple[0]).read()  % {
 		'date_to':DATE_TO
 		}
 	query_result = dc.select(query,strip=True)
@@ -34,20 +39,17 @@ def get_and_set_data(worksheet_dict):
 
 
 if __name__ == '__main__':
-	print(sys.argv)
-	# workbook_name = workbook_tuple[0]
-	# workbook_dict = workbook_tuple[1]
-	# print("Running %s..." % workbook_name,end='')
-	# print(workbook_dict)
-	# print("Done!")
+	print("Running %s..." % WORKBOOK_NAME,end='')
 
-	# workbook = shadow_google_spreadsheet.open(WORKBOOK_NAME)
-	# dc = DatabaseConnection()
+	workbook = shadow_google_spreadsheet.open(WORKBOOK_NAME)
+	dc = DatabaseConnection()
 
-	# Single Thread:
-	# for worksheet_dict in WORKSHEET_LIST:
-		# get_and_set_data(worksheet_dict)
+	if MULTI_THREAD == "True":
+		thread_count = len(WORKSHEET_LIST) if MAX_THREADS == "0" else int(MAX_THREADS)
+		with Pool(thread_count) as p:
+			result = p.map(get_and_set_data, WORKSHEET_LIST.items())
+	else:
+		for worksheet_tuple in WORKSHEET_LIST.items():
+			get_and_set_data(worksheet_tuple)
 
-	# Multi Threading:
-	# with Pool(len(WORKSHEET_LIST)) as p:
-		# result = p.map(get_and_set_data, WORKSHEET_LIST)
+	print("Done!")
