d361dfc8c26af76a46c55021c9b1cd8100e2b6a3
Victor <victor@marciamello.com.br>
2019-01-08 16:33:17 -0200

    GDOCS Outputs

--- pentaho/python_bi_gdocs_outputs/run.py
+++ pentaho/python_bi_gdocs_outputs/run.py
@@ -8,22 +8,18 @@ PARAMETERS = {
 	"max_threads":0, # 0 if threads = len(data)
 	"date_to":"GETDATE()-1",
 	"workbook_name":"Daily Sales",
-	"worksheet_list":{
-		"daily_sales/sales_data_mtd.sql":"SalesDataMTD",
-		"daily_sales/sales_data_week.sql":"SalesDataWEEK",
-		"daily_sales/sellers_data_mtd.sql":"SellerDataMTD",
-		"daily_sales/sellers_data_week.sql":"SellerDataWEEK",
-		"daily_sales/sales_orders.sql":"SalesOrders",
-		"daily_sales/stock_data.sql":"StockData",
-		"daily_sales/stock_data_wo_store.sql":"StockDataWOStore",
-		"daily_sales/receivings_data.sql":"ReceivingsData",
-		"daily_sales/receivings_data_wo_store.sql":"ReceivingsDataWOStore"
-	}
+"worksheet_list":{
+	"daily_sales/sales_data_mtd.sql":"SalesDataMTD",
+	"daily_sales/sales_data_week.sql":"SalesDataWEEK",
+	"daily_sales/sellers_data_mtd.sql":"SellerDataMTD",
+	"daily_sales/sellers_data_week.sql":"SellerDataWEEK",
+	"daily_sales/sales_orders.sql":"SalesOrders",
+	"daily_sales/stock_data.sql":"StockData",
+	"daily_sales/stock_data_wo_store.sql":"StockDataWOStore",
+	"daily_sales/receivings_data.sql":"ReceivingsData",
+	"daily_sales/receivings_data_wo_store.sql":"ReceivingsDataWOStore"
 }
-
-
-workbook = shadow_google_spreadsheet.open(WORKBOOK_NAME)
-dc = DatabaseConnection()
+}
 
 def get_and_set_data(worksheet_dict):
 	print('Updating sheet: %s...' % worksheet_dict['worksheet_name'],end='')
@@ -45,6 +41,8 @@ if __name__ == '__main__':
 	# print(workbook_dict)
 	# print("Done!")
 
+	# workbook = shadow_google_spreadsheet.open(WORKBOOK_NAME)
+	# dc = DatabaseConnection()
 
 	# Single Thread:
 	# for worksheet_dict in WORKSHEET_LIST:
